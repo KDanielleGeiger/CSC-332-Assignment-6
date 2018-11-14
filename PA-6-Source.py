473b718e072dfd64c923b2229415d3e3c4636537
KDanielleGeiger <42385182+KDanielleGeiger@users.noreply.github.com>
2018-11-14 16:13:13 -0600

    Added matrix image

--- PA-6-Source.py
+++ PA-6-Source.py
@@ -1,6 +1,8 @@
 import tkinter.ttk
 from tkinter import *
 from functools import partial
+import matplotlib.pyplot as plot
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 seqText = 'Enter a sequence (Ex: AABCC)'
 matchText = 'Enter match (Int ≥ 0)'
@@ -11,51 +13,62 @@ errorLbl = None
 
 def main():
     window = Tk()
+    window.geometry("520x320")
     window.title("Global Sequence Alignment")
+    window.grid_rowconfigure(0, weight=1)
+
+    ##  Create frames
+    frameLeft = Frame(window)
+    frameLeft.grid(row=0, column=0, sticky=NS)
+    frameMid = Frame(window)
+    frameMid.grid(row=0, column=1, sticky=NS)
+    frameMid.rowconfigure(0, weight=1)
+    frameRight = Frame(window)
+    frameRight.grid(row=0, column=2, sticky=NS)
 
     ##  Entry for the first sequence
-    seqEntry1 = Entry(window, fg='light grey', relief=FLAT, width=26)
+    seqEntry1 = Entry(frameLeft, fg='light grey', relief=FLAT, width=26)
     seqEntry1.insert(0, seqText)
-    seqEntry1.grid(row=1, column=0, pady=(10,0), padx=(10,10))
+    seqEntry1.grid(row=1, column=0, pady=(60,0), padx=(10,10))
     seqEntry1.bind('<FocusIn>', partial(onFocusIn, seqEntry1))
     seqEntry1.bind('<FocusOut>', partial(onFocusOut, seqEntry1, 'S'))
 
     ##  Entry for the second sequence
-    seqEntry2 = Entry(window, fg='light grey', relief=FLAT, width=26)
+    seqEntry2 = Entry(frameLeft, fg='light grey', relief=FLAT, width=26)
     seqEntry2.insert(0, seqText)
     seqEntry2.grid(row=2, column=0, pady=(5,0), padx=(10,10))
     seqEntry2.bind('<FocusIn>', partial(onFocusIn, seqEntry2))
     seqEntry2.bind('<FocusOut>', partial(onFocusOut, seqEntry2, 'S'))
 
     ##  Match entry
-    match = Entry(window, fg='light grey', relief=FLAT, width=26)
+    match = Entry(frameLeft, fg='light grey', relief=FLAT, width=26)
     match.insert(0, matchText)
     match.grid(row=3, column=0, pady=(5,0), padx=(10,10))
     match.bind('<FocusIn>', partial(onFocusIn, match))
     match.bind('<FocusOut>', partial(onFocusOut, match, 'M'))
 
     ##  Mismatch entry
-    mismatch = Entry(window, fg='light grey', relief=FLAT, width=26)
+    mismatch = Entry(frameLeft, fg='light grey', relief=FLAT, width=26)
     mismatch.insert(0, mismatchText)
     mismatch.grid(row=4, column=0, pady=(5,0), padx=(10,10))
     mismatch.bind('<FocusIn>', partial(onFocusIn, mismatch))
     mismatch.bind('<FocusOut>', partial(onFocusOut, mismatch, 'I'))
 
     ##  Gap entry
-    gap = Entry(window, fg='light grey', relief=FLAT, width=26)
+    gap = Entry(frameLeft, fg='light grey', relief=FLAT, width=26)
     gap.insert(0, gapText)
     gap.grid(row=5, column=0, pady=(5,0), padx=(10,10))
     gap.bind('<FocusIn>', partial(onFocusIn, gap))
     gap.bind('<FocusOut>', partial(onFocusOut, gap, 'G'))
 
     ##  Submit button
-    submitBtn = Button(window, text='Submit', cursor='hand2')
-    submitBtn.config(command=partial(submit, window, seqEntry1, seqEntry2, match, mismatch, gap))
+    submitBtn = Button(frameLeft, text='Submit', cursor='hand2')
+    submitBtn.config(command=partial(submit, frameLeft, frameRight, seqEntry1, seqEntry2, match, mismatch, gap))
     submitBtn.grid(row=6, column=0, pady=(5,0), padx=(10,10))
 
     ##  Separator
-    sep = ttk.Separator(window, orient=VERTICAL)
-    sep.grid(row=0, column=1, rowspan=8, pady=(5,5), padx=(10,10), sticky=NS)
+    sep = ttk.Separator(frameMid, orient=VERTICAL)
+    sep.grid(row=0, column=1, pady=(20,20), padx=(10,10), sticky=NS)
 
     window.mainloop()
 
@@ -108,7 +121,7 @@ def checkEntries(seqEntry1, seqEntry2, match, mismatch, gap):
     return valid, err
 
 ##  Remove any existing errors and display the new error (if there is one)
-def displayError(window, valid, err):
+def displayError(frameLeft, valid, err):
     global errorLbl
     
     if errorLbl is not None:
@@ -118,7 +131,7 @@ def displayError(window, valid, err):
         errVar = StringVar()
         errVar.set(err)
 
-        errorLbl = Label(window, textvariable=errVar, fg='red', wraplength=160)
+        errorLbl = Label(frameLeft, textvariable=errVar, fg='red', wraplength=160)
         errorLbl.grid(row=7, column=0, pady=(10,0))
 
 ##  Each element in the matrix is a Cell object
@@ -187,18 +200,58 @@ def fillMatrix(matrix, match, mismatch, gap, seq1, seq2):
 
     return matrix
 
+##  Create image of matrix and display it in the UI
+def createPlot(frameRight, matrix, seq1, seq2):
+    ##  Add extra space to sequences
+    seq1 = '-' + seq1
+    seq2 = '-' + seq2
+
+    figure = plot.figure()
+    chart = figure.add_subplot(111)
+
+    ##  Create and format ticks
+    xlim = len(seq1)
+    ylim = len(seq2)
+
+    chart.set_xlim(left=0, right=xlim)
+    chart.set_ylim(bottom=ylim, top=0)
+    chart.xaxis.tick_top()
+    chart.set_xticks(range(0, xlim, 1))
+    chart.set_yticks(range(0, ylim, 1))
+    chart.set_xticks([i+0.5 for i in range(0, xlim)], minor=True)
+    chart.set_yticks([i+0.5 for i in range(0, ylim)], minor=True)
+    chart.xaxis.set_major_formatter(plot.NullFormatter())
+    chart.yaxis.set_major_formatter(plot.NullFormatter())
+    chart.set_xticklabels(list(seq1), minor=True)
+    chart.set_yticklabels(list(seq2), minor=True)
+    chart.tick_params(axis='both', which='minor', length=0, width=0)
+    chart.grid(True)
+
+    ##  Add values of matrix in plot
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            x = i + 0.5
+            y = j + 0.5
+            chart.text(x, y, matrix[i][j].value, ha='center', va='center', color='blue')
+
+    ##  Display the plot in the UI
+    canvas = FigureCanvasTkAgg(figure, master=frameRight)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=0, column=2, padx=(10,10), pady=(10,0))
+    canvas.get_tk_widget().config(width=300, height=300)
+
 ##  Submit input and run the algorithm
-def submit(window, seqEntry1, seqEntry2, match, mismatch, gap):
+def submit(frameLeft, frameRight, seqEntry1, seqEntry2, match, mismatch, gap):
     ##  Check user input
     valid, err = checkEntries(seqEntry1, seqEntry2, match, mismatch, gap)
-    displayError(window, valid, err)
+    displayError(frameLeft, valid, err)
 
     if valid == True:
         ##  Initialize and fill the matrix
         matrix = initializeMatrix(seqEntry1.get(), seqEntry2.get(), int(gap.get()))
         matrix = fillMatrix(matrix, int(match.get()), int(mismatch.get()),
                             int(gap.get()), seqEntry1.get(), seqEntry2.get())
-        print(matrix)
+        createPlot(frameRight, matrix, seqEntry1.get(), seqEntry2.get())
 
 if __name__ == '__main__':
     main()
